48aa1339d7323519f264d30bf8331a0b6781cfa9
zhangshu <zhangshuhong888@qq.com>
2020-04-09 12:12:22 +0800

    v1

--- script/推荐/循环滑动停止加减速.js
+++ script/推荐/循环滑动停止加减速.js
@@ -14,20 +14,33 @@ var sleepr=function(short,long){
     show("等待:"+rt +" 毫秒")
     sleep(rt)
 }
+var ratio=100
 var gfw
 var  creatgfloatywindow=function(){
     gfw=floaty.rawWindow(
         <horizontal >
-           <text id="stop" w="45" h="45" gravity="center" textSize="18sp" background="#55ff0000">停止</text>
-        <text id="text" w="*" h="*" gravity="center" textSize="18sp" background="#55ffff00">提醒</text>
+           <text id="stop" w="45" h="45" gravity="center" textSize="18sp" background="#55ff0000" >停止</text>
+           <text id="jiasu" w="45" h="45" gravity="center" textSize="16sp" background="#55ff0000" >加速</text>
+           <text id="jiansu" w="45" h="45" gravity="center" textSize="16sp" background="#55ff0000">减速</text>
+            <text id="text" w="*" h="*" gravity="center" textSize="18sp" background="#55ffff00">提醒</text>
         </horizontal>
         
     );
     gfw.setSize(device.width, 120)
     
-    gfw.setPosition(0,85)
+    gfw.setPosition(0,80)
     gfw.stop.on("click",function(){
         engines.stopAllAndToast()
+    })
+    gfw.jiasu.on("click",function(){
+        ratio=ratio+10
+    })
+    gfw.jiansu.on("click",function(){
+        
+        ratio=ratio-10
+        if(ratio<10){
+            ratio=10
+        }
     })
 }
 
@@ -36,7 +49,6 @@ var show=function(txt){
       creatgfloatywindow()
     }
     ui.run(function(){
-     
         gfw.text.setText(txt)
      })
 
@@ -49,6 +61,9 @@ var 下滑=function(){
     滑动(20,10,3,13,17,500,500)
 }
 滑动次数=0
+floaty.closeAll()
+engines.stopOther()
+home()
 while(true){
     if(滑动次数%10==0){
         下滑()
@@ -73,8 +88,7 @@ while(true){
     }else {
         上滑()
     }
-    
-    sleepr(8000,15000)
+    sleepr(8000/100*ratio,12000/100*ratio)
     滑动次数=滑动次数+1
 }
 
