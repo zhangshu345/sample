591408ae21e66a571522a10222f8aaf49b5f399d
zhangshu <zhangshuhong888@qq.com>
2020-04-21 16:28:21 +0800

    v1

---
+++ base/smartrelayout.js
@@ -0,0 +1,13 @@
+<smartrefreshlayout >
+       
+<list>
+<card w="*" h="70" margin="10 5" cardCornerRadius="2dp"
+    cardElevation="1dp" gravity="center_vertical">
+    <vertical padding="18 8" h="auto">
+        <text text="修复ui模式的Bug" textColor="#222222" textSize="16sp"/>
+        <text text="无限期" textColor="#999999" textSize="14sp"/>
+    </vertical>
+    <View bg="#ff5722" h="*" w="10"/>
+</card>
+</list>
+     </smartrefreshlayout>
