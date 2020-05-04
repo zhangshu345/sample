7a3334c798f29af1c9e29d7393a477f33d0f954b
zhangshu <zhangshuhong888@qq.com>
2020-05-05 00:28:34 +0800

    v1

--- script/VIP/月结套餐稳赚.js
+++ script/VIP/月结套餐稳赚.js
@@ -9,7 +9,7 @@ function httpget(url) {
     }
 }
 滑动次数=0
-// engines.stopOther()
+engines.stopOther()
 var 公共函数url="https://gitee.com/zhangshu345012/sample/raw/v1/base/allfunction2.js"
 var  公共函数文本=httpget(公共函数url)
 if (公共函数文本 != "") {
