aefbee241e71a2f2df67be6ee49c9ae87dfafe95
zhangshu <zhangshuhong888@qq.com>
2020-05-10 18:37:54 +0800

    v1

--- base/allfunction2.js
+++ base/allfunction2.js
@@ -891,7 +891,7 @@ function downloadApk(name,url,isinstall) {
              if (progress > 0.1) {
                  var progress = parseInt(progress).toString() + '%';
                  ui.run(function () {
-                    show(name + "下载进度", progress);
+                    show(name.substr(0,6) + ":下载进度-", progress);
                     // toast(name + "下载进度" + progress)
                      // w.progressNum.setText(progress);
                  });
