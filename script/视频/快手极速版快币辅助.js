f5bf737d4a522005df01690e3e636868bb3a78f4
zhangshu <zhangshuhong888@qq.com>
2020-05-30 11:55:14 +0800

    V1

--- script/视频/快手极速版快币辅助.js
+++ script/视频/快手极速版快币辅助.js
@@ -18,20 +18,23 @@ toastLog("公共函数实例化成功")
 }else {
 toastLog("公共函数实例化失败,程序返回")
 }
-[["属性","值"],["accessibilityFocused","false"],["bounds","(301,1120,778,1193)"],["checked","false"],["className","android.widget.TextView"],["clickable","false"],["column","-1"],["columnCount","0"],["columnSpan","-1"],["contextClickable","false"],["depth","4"],["desc",""],["dismissable","false"],["drawingOrder","4"],["editable","false"],["enabled","true"],["focusable","false"],["fullId","com.smile.gifmaker:id/live_red_packet_message_view"],["id","live_red_packet_message_view"],["idHex","0x7f09166a"],["indexInParent","3"],["longClickable","false"],["packageName","com.smile.gifmaker"],["row","-1"],["rowCount","0"],["rowSpan","-1"],["scrollable","false"],["selected","false"],["text","手慢了，红包派完了"]]
+var appliveactivity="com.kuaishou.live.core.basic.activity.LivePlayActivity"
 
-var 快手弹窗倒计时id="com.smile.gifmaker:id/count_down_view"
-var 快手弹窗状态="com.smile.gifmaker:id/live_red_packet_pre_snatch_state_view"
-var 快手弹窗快币抢到后数量id="com.smile.gifmaker:id/coin_num_view"
-var 快手直播页红包标志="com.smile.gifmaker:id/live_arrow_red_packet_pendant_icon_view"
-var 快手直播退出标志="com.smile.gifmaker:id/live_close_place_holder"
-var 快手直播关闭按钮="com.smile.gifmaker:id/close_icon_view"
-var 快手直播页红包背景="com.smile.gifmaker:id/background_view_normal"
-var 快手直播页主播姓名="com.smile.gifmaker:id/live_name_text"
-var 快手直播红包金额数="com.smile.gifmaker:id/live_red_packet_coin_num_view"
-var 快手直播页红包集合=[快手直播页红包标志,快手直播页红包背景,]
-var 快手直播退出标志集合=[快手直播退出标志,快手直播关闭按钮]
-var 快手直播间标志集合=[快手直播页主播姓名,快手直播退出标志]
+var 快手极速版弹窗主播名称id="com.kuaishou.nebula:id/live_red_packet_name_view"  // text 主播名称 
+var 快手极速版弹窗状态=""
+var 快手极速版弹窗快币抢到后数量id=""
+var 快手极速版直播页红包标志="com.kuaishou.nebula:id/background_view_normal"
+var 快手极速版直播页红包倒计时="com.kuaishou.nebula:id/live_arrow_red_packet_pendant_state_text_view"  
+var 快手极速版直播页弹窗红包倒计时="com.kuaishou.nebula:id/count_down_view"  // 5秒后 4分钟后 android.widget.TextView  "depth","7" "columnSpan","-1" "packageName","com.kuaishou.nebula" "indexInParent","0"
+var 快手极速版直播退出标志=""
+var 快手极速版直播关闭按钮=""
+var 快手极速版直播页红包背景=""
+var 快手极速版直播页主播姓名=""
+var 快手极速版直播红包金额数="com.kuaishou.nebula:id/live_red_packet_coin_num_view"   //直接数量 "depth","4"
+var 快手极速版直播红包状态="com.kuaishou.nebula:id/live_red_packet_pre_snatch_state_view"  //可以点击
+var 快手极速版直播页红包集合=[快手极速版直播红包状态,快手极速版直播页红包背景]
+var 快手极速版直播退出标志集合=[快手极速版直播退出标志,快手极速版直播关闭按钮]
+var 快手极速版直播间标志集合=[快手极速版弹窗主播名称id,快手极速版直播退出标志]
 var x=500
 var y=1376
 var 按压时间=3
@@ -39,12 +42,12 @@ var 提前时间=2000
 var 循环次数=1000
 var 获取倒计时=function(){
     show("开始获取倒计时")
-    node_coin=id(快手直播红包金额数).findOne(100)
+    node_coin=id(快手极速版直播红包金额数).findOne(100)
     if(node_coin){
         n_coin=parseInt(node_coin.text())
         show("金币数:"+n_coin)
     }
-    node_count=id(快手弹窗倒计时id).findOne(100)
+    node_count=id(快手极速版直播页弹窗红包倒计时).findOne(100)
     if(node_count){
         x=node_count.bounds().centerX()
         y=node_count.bounds().centerY()
@@ -61,15 +64,16 @@ var 获取倒计时=function(){
             sleep(n_count*1000-提前时间)
             show("开始点击")
             循环次数=提前时间/(按压时间*2)
+            djcs=0
             for(var i = 0; i < 循环次数; i++){
              //点击位置(500, 1000), 每次用时1毫秒
              press(x, y, 按压时间);
+             djcs=djcs+1
              }
-             show("结束点击")
+             show("结束点击:"+djcs)
         }else{
             show("时间过长")
         }
-
     }
 }
 
@@ -80,10 +84,9 @@ var 获取倒计时=function(){
 engines.stopOther()
 creatsetfloatywindow()
 进直播间次数=0
-show("快手抢快币")
+show("快手极速版抢快币")
 gfw.setPosition(0,device.height*6/7)
 while(true){
-
     获取倒计时()
     sleep(4000)
 }
