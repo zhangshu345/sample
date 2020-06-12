8f670b669a05379af73276f76d64c1dd456888c4
zhangshu <zhangshuhong888@qq.com>
2020-06-12 21:44:21 +0800

    v1

--- base/调试链接.js
+++ base/调试链接.js
@@ -1,2 +1,7 @@
 importClass(com.hongshu.androidjs.core.debug.DevPluginService)
-DevPluginService.getInstance().connectToServer("192.168.3.2");
+// DevPluginService.getInstance().connectToServer("192.168.3.2");
+var  todevelopersetting=function(){
+    let i = app.intent({ action: "android.settings.APPLICATION_DEVELOPMENT_SETTINGS", 
+    flags:["activity_new_task"] // data: "file:///sdcard/1.png"  
+});}
+todevelopersetting()
