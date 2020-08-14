e81d925917dfd9601293ece9699dc94af2027b13
zhangshu <zhangshuhong888@qq.com>
2020-08-14 16:44:47 +0800

    v2

--- script/sample/媒体编辑/视频压缩/视频压缩.js
+++ script/sample/媒体编辑/视频压缩/视频压缩.js
@@ -38,7 +38,6 @@ function 视频变速(视频路径,保存路径,速度){
     let 视频变速命令="ffmpeg -y -i 原视频 -filter_complex [0:v]setpts=PTS/速度[v];[0:a]atempo=速度[a] -map [v] -map [a] -preset superfast 新视频".replace("原视频",视频路径).replace("新视频",保存路径).replace("速度",""+速度);
   log(视频变速命令)
     execMediaEditCommand("变速",视频变速命令)
-  
     // execMediaEditCommand("变速",视频变速命令,function(){
     //     function onFinish(){
     //         toastLog(action+"完成")
@@ -56,4 +55,5 @@ function 视频变速(视频路径,保存路径,速度){
 }
 
 // 视频变速(rootpath+"/刷宝.mp4",rootpath+"/新刷宝.mp4",0.5)
-execMediaEditCommand("变速","ffmpeg -y -ss 0 -t 7 -i /storage/emulated/0/刷宝.mp4 -r 5 -s 280x606 -preset superfast /storage/emulated/0/result.gif")
+
+execMediaEditCommands("变速",["ffmpeg -y -ss 0 -t 7 -i /storage/emulated/0/快手.mp4 -r 5 -s 280x606 -preset superfast /storage/emulated/0/快手.gif"])
