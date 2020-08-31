d205512f3a2f1c490d0f29b57df592de75f7c1bd
zhangshu <zhangshuhong888@qq.com>
2020-08-31 18:10:43 +0800

    v2

--- config/scripts/生成列表.js
+++ config/scripts/生成列表.js
@@ -46,18 +46,25 @@ for(var i=0; i<data.class.length; i++)
 savejson('test.json', data)
 }
 var scriptobj={name:"脚本模板",
-desc:{summary:"脚本模板简介",html:""},
+desc:{summary:"脚本模板简介",html:"",script:""},
 tags:["推荐"],
+devices:["xiaomi","samsung","HONOR","Meizu","OPPO"],
 features:[],
+open:true,
+install:true,
 keys:[{name:"测试",value:"值"},{name:"测试2",value:"值2"}],
 layout:{type:1,orientation:1,width:0,height:0,back:"@color/red",fore:"",reverselayout:false,size:0,textsize:6,buttons:[{name:"下载",eventid:-1,script:"",uipath:"",icon:"",key:""}]},
-pay:{coin:0,money:0,daymoney:0,monthmoney:0,yearmoney:0},
+pay:{coin:0,daycoin:0,monthcoin:0,yearcoin:0,money:0,daymoney:0.01,monthmoney:0.01,yearmoney:0.01},
 download:false,
-runconfig:{starttime:"5:24",time:3600,perhormoney:0.65,daymaxmoney:2,onetime:1800
-,logintype:"phone",daycashcondition:0.3,daymoneyinterval:7},
+runconfig:{starttime:"5:24",endtime:"0:0",runtime:3600,seenews:0,seevideo:0,seeshortvideo:0,
+sign:true,time:3000,devicemanager:false,accessibility:true,systemsetting:false,notificationmanager:false,
+floatyposition:"0,120,400,220",showfloaty:true,checkbattery:30,onlyscript:false,scriptchannel:"all",
+onetime:3600,maxtime:10000,logintype:"phone",tomoney:"weixin",tomoneyaccount:"zfb|姓名|提现账号",
+daycashcondition:0.3,daycashcondition:0.3,daycoincondition:3000,bigmoneytomoneyday:7,bigmoney:5.1
+},
 root:false,password:"123456781234567812345678",key:"1234567887654321",code:"666666",
 reward:{coin:0,"money":0,experience:10,achievement:"成就"},
-money:{perdaymoney:0.38,permonthmoney:60.1,peryearmoney:360.0},
+money:{perhourmoney:0.3,perdaymoney:0.38,permonthmoney:60.1,peryearmoney:360.0},
 path:"http://zhangshuhong888.iask.in:8989/加密脚本/加密推荐上滑.js",
 source:1,
 author:{name:"作者",id:"",icon:"",qq:"11226677",weixin:"jiajiajia",phone:"1871768888"},
@@ -66,7 +73,7 @@ version:0,
 appversioni:10,
 requestapi:10,
 icon:"",
-app:{name:"",pkg:"",icon:"",downloadurl:"", version:""}
+app:{name:"",pkg:"",icon:"",downloadurl:"", version:"",versioni:0}
 }
 
 function createconfigjsonfile(filedir,filename,data){
