0852342136bbda5f55f937ad72e2f72c22464ef3
zhangshu <zhangshuhong888@qq.com>
2020-09-11 17:32:10 +0800

    v2

--- config/邀请配置/执行应用邀请.js
+++ config/邀请配置/执行应用邀请.js
@@ -1,3 +1,8 @@
+
 function execappinvite(appname,channel){
-    let appinvitecodeurl=
+    if(!appname){
+        return false
+    }
+    channel=channel||"defualt"
+    let appinvitecodeurl="https://gitee.com/zhangshu345012/sample/raw/v2/config/邀请配置/"+channel+"_appinvite.json"
 }
