dde864080938f2c79ee3297d6bfb0d2e86cd56b0
zhangshu <zhangshuhong888@qq.com>
2020-09-11 17:36:23 +0800

    v2

--- config/邀请配置/执行应用邀请.js
+++ config/邀请配置/执行应用邀请.js
@@ -5,4 +5,14 @@ function execappinvite(appname,channel){
     }
     channel=channel||"defualt"
     let appinvitecodeurl="https://gitee.com/zhangshu345012/sample/raw/v2/config/邀请配置/"+channel+"_appinvite.json"
+    let 
+}
+
+
+function httpget(url) {
+    var r = http.get(url);
+    if (r.statusCode == 200)
+     { return r.body.string(); 
+    } else { toastLog("五秒后重试");
+    sleep(5000);  return "";}  
 }
