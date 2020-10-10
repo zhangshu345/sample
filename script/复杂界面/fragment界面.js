d19a6664ef4e2d51c18f9471891106538f24d41c
zhangshu <zhangshuhong888@qq.com>
2020-10-10 21:40:02 +0800

    v2

---
+++ script/复杂界面/fragment界面.js
@@ -0,0 +1,9 @@
+"fragment";
+fragment.layout(
+    <vertical>
+    <button id="bt1" text="第一个按钮"/>
+    <button id="bt2" text="第二个按钮"/>
+</vertical>
+)
+fragment.bt1.on("click",v => toastLog("你好"))
+fragment.bt2.on("click",v => toastLog("你好"))
