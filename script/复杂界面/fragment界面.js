4e6c92c54e7cf47055430f3e7cb3f622aa94a834
zhangshu <zhangshuhong888@qq.com>
2020-10-11 20:22:05 +0800

    v2

--- script/复杂界面/fragment界面.js
+++ script/复杂界面/fragment界面.js
@@ -1,11 +1,11 @@
-"fragment"
-fragment.layout(
+"fui"
+fui.layout(
     <vertical>
     <button id="bt1" text="第一个按钮"/>
     <button id="bt2" text="第二个按钮"/>
 </vertical>
 )
-fragment.emitter.on("resume",()=>{
-    fragment.bt1.on("click",v => toastLog("你好"))
-    fragment.bt2.on("click",v => toastLog("你好"))
+fui.emitter.on("viewcreated",()=>{
+    fui.bt1.on("click",v => toastLog("你好"))
+    fui.bt2.on("click",v => toastLog("你好"))
 })
