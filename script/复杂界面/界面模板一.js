1b82cb05272181cf71d261a1694d9c77ae6a7356
zhangshu <zhangshuhong888@qq.com>
2020-10-12 17:04:36 +0800

    v2

--- script/复杂界面/界面模板一.js
+++ script/复杂界面/界面模板一.js
@@ -50,7 +50,7 @@ ui.emitter.on("options_item_selected", (e, item)=>{
             aroutergourl("\/script\/usermain") 
             break;
         case "关于":
-            alert("关于", "Auto.js界面模板 v1.0.0");
+            alert("关于", "AutoTool界面模板 v1.0.0");
             break;
     }
     e.consumed = true;
