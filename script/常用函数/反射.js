3fb8557639ebc77dc1844f7aeb257208fd885080
zhangshu <zhangshuhong888@qq.com>
2020-10-12 14:19:01 +0800

    v2

---
+++ script/常用函数/反射.js
@@ -0,0 +1,24 @@
+log(device)
+log("runtime:class:"+runtime.getClass())
+log("accessibilityBridge:class:"+runtime.accessibilityBridge.getClass())
+log("config：class："+runtime.accessibilityBridge.getConfig().getClass())
+// var fields=runtime.accessibilityBridge.getConfig().getClass().getFields()
+var fields=runtime.accessibilityBridge.getConfig().getClass().getDeclaredFields()
+fields.forEach(f => {
+    f.setAccessible(true);
+    type=f.getGenericType().toString()
+    log("config 属性："+f.getName()+"--"+type)
+});
+// var funs=runtime.accessibilityBridge.getConfig().getClass().getMethods()
+// funs.forEach(f =>{
+//     log("函数:"+f.toString())
+// })
+// var conss=runtime.accessibilityBridge.getConfig().getClass().getConstructors();
+// conss.forEach(c =>{
+//     log("构造方法："+c.toString())
+// })
+
+let ss =runtime.accessibilityBridge.getConfig().֏
+ss.forEach(s =>{
+    log(s)
+})
