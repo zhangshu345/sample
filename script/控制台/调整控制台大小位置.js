3fd53b1139187d2e89a1c4609321377d8ac1b4be
zhangshu <zhangshuhong888@qq.com>
2020-11-03 16:35:59 +0800

    布局分析 合并之前

--- script/控制台/调整控制台大小位置.js
+++ script/控制台/调整控制台大小位置.js
@@ -3,6 +3,10 @@ console.log("调整大小...");
 console.setSize(1000, 1000);
 sleep(2000);
 console.log("调整位置...");
-console.setPosition(0, 500);
+// console.setPosition(0, 500);
 sleep(2000);
-console.hide();
+// console.hide();
+
+setInterval(() => {
+    
+}, 1000);
