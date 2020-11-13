1355dfa49d4d9f2e98791cbcdb854a1a1a2ba3e0
zhangshu <zhangshuhong888@qq.com>
2020-11-13 18:28:03 +0800

    1.1.2

---
+++ script/常用函数/抖音相关.js
@@ -0,0 +1,2 @@
+https://www.iesdouyin.com/share/video/6893854097804446989/?region=CN&mid=6889652670727047950&u_code=187md2c6m&titleType=title&did=60092919754&iid=773690567430447&utm_source=copy_link&utm_campaign=client_share&utm_medium=android&app=aweme
+https://www.iesdouyin.com/share/video/6894190064964783367/?region=CN&mid=6884217113256397575&u_code=187md2c6m&titleType=title&did=60092919754&iid=773690567430447&utm_source=copy_link&utm_campaign=client_share&utm_medium=android&app=aweme
