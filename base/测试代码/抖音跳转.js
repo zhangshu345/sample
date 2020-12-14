ad3f66dfe9c9f15b038a3f3c3395e39202d1b5ab
zhangshu <zhangshuhong888@qq.com>
2020-12-14 21:50:22 +0800

    v2

---
+++ base/测试代码/抖音跳转.js
@@ -0,0 +1,4 @@
+// com.hongshu.utils.IntentUtils.goKuaiShowHomePage(context)
+
+// com.hongshu.utils.IntentUtils.goDouyinVideoPage(context,"6906053018739920140")
+com.hongshu.utils.IntentUtils.goDouyinUserPage(context,"106731108401")
