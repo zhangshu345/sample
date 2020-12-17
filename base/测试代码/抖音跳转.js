f3b69ba3e0ebd3cc4663be69b29d498b23d1998e
zhangshu <zhangshuhong888@qq.com>
2020-12-18 00:15:07 +0800

    v2

--- base/测试代码/抖音跳转.js
+++ base/测试代码/抖音跳转.js
@@ -1,4 +1,12 @@
 // com.hongshu.utils.IntentUtils.goKuaiShowHomePage(context)
 
 // com.hongshu.utils.IntentUtils.goDouyinVideoPage(context,"6906053018739920140")
-com.hongshu.utils.IntentUtils.goDouyinUserPage(context,"106731108401")
+// com.hongshu.utils.IntentUtils.goDouyinUserPage(context,"106731108401")
+
+let content=com.blankj.utilcode.util.CacheDiskUtils.getInstance("data_helpview").getString("data")
+log(content)
+
+com.blankj.utilcode.util.CacheDiskUtils.getInstance("data_helpview").remove("data")
+
+ content=com.hongshu.config.AppConfigManager.Companion.getAppconfigmanager().getKeyValue("helpurl")
+log(content)
