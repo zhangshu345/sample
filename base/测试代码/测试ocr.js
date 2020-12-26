0bc52102aa7efb937069991fea09a4280349b1c3
zhangshu <zhangshuhong888@qq.com>
2020-12-26 19:09:11 +0800

    v2

--- base/测试代码/测试ocr.js
+++ base/测试代码/测试ocr.js
@@ -1,5 +1,7 @@
  const chisimvertraineddata="http://zhangshuhong888.iask.in:8989/tessdata_best/chi_sim_vert.traineddata";
- const chisimtraineddata="http://zhangshuhong888.iask.in:8989/tessdata_best/chi_sim.traineddata";
+ const chisimtraineddata="http://zhangshuhong888.iask.in:8989/tessdata_best/chi_sim.traineddata"
+ //"https://gitee.com/zhangshu345012/sample/raw/v2/data/tessdata_best/chi_sim.traineddata"
+ //"http://zhangshuhong888.iask.in:8989/tessdata_best/chi_sim.traineddata";
 
 files.ensureDir("/sdcard/tessdata/")
 if(!files.exists("/sdcard/tessdata/chi_sim.traineddata")){
@@ -15,7 +17,7 @@ if(!files.exists("/sdcard/tessdata/chi_sim.traineddata")){
 importClass(com.hongshu.autotools.core.runtime.api.Ocr)
 var ocr =new Ocr();
 log(files.getSdcardPath())
-ocr.init(files.getSdcardPath(),"chi_sim")
-var s=ocr.ocrFile(files.getSdcardPath()+"/DCIM/Screenshots/你就.jpg")
-log("识别结果:"+s)
+ocr.init("/sdcard","chi_sim")
+var s=ocr.ocrFile("/sdcard/DCIM/Screenshots/你就.jpg")
+log("识别结果:"+s.replace(" ",""))
 
