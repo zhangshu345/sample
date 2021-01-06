63a1d58db87dd800a4a2f3d5c538ba9c96962c74
zhangshu <zhangshuhong888@qq.com>
2021-01-06 13:49:09 +0800

    v2

--- base/测试代码/测试获取邀请码.js
+++ base/测试代码/测试获取邀请码.js
@@ -1,1 +1,15 @@
-log(getInviteCode("com.kuaishou.nebula"))
+
+// let d=getInviteCode("com.ss.android.ugc.aweme.lite")
+// log(d)
+// let invitemanager=com.hongshu.config.InviteActionManager.getInstance()
+// invitemanager.updateAppInvite
+// log(invitemanager.pkgset)
+// sleep(3000)
+// log(invitemanager.pkgset)
+// sleep(3000)
+// log(invitemanager.pkgset)
+// sleep(3000)
+// log(invitemanager.pkgset)
+
+let appconfig=com.hongshu.config.AppConfigManager.Companion.getAppconfigmanager()
+log(appconfig.getAppConfig().getInviteactions())
