4e264300609c72bd1b600f9c678139b8feba8f04
zhangshu <zhangshuhong888@qq.com>
2021-01-06 15:06:45 +0800

    v2

--- base/测试代码/测试获取邀请码.js
+++ base/测试代码/测试获取邀请码.js
@@ -1,15 +1,15 @@
 
-// let d=getInviteCode("com.ss.android.ugc.aweme.lite")
-// log(d)
-// let invitemanager=com.hongshu.config.InviteActionManager.getInstance()
-// invitemanager.updateAppInvite
-// log(invitemanager.pkgset)
-// sleep(3000)
-// log(invitemanager.pkgset)
-// sleep(3000)
-// log(invitemanager.pkgset)
-// sleep(3000)
-// log(invitemanager.pkgset)
+ let d=getInviteCode("com.ss.android.ugc.aweme.lite")
+ log(d)
+let invitemanager=com.hongshu.config.InviteActionManager.getInstance()
+log(invitemanager.defualtinviteactionHashMap.get("com.ss.android.ugc.aweme.lite"))
+log(invitemanager.getAppInviteAction("com.ss.android.ugc.aweme.lite"))
 
 let appconfig=com.hongshu.config.AppConfigManager.Companion.getAppconfigmanager()
-log(appconfig.getAppConfig().getInviteactions())
+let s=appconfig.getAppConfig().getInviteactions()
+log(s)
+// invitemanager.setDefualtInviteActions(s)
+
+// sleep(3000)
+log(invitemanager.pkgset)
+log(invitemanager.pkgset.contains("com.ss.android.ugc.aweme.lite"))
