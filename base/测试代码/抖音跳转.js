5ec57678a533d6cc8b1a5188a46ebb8e038cd61d
zhangshu <zhangshuhong888@qq.com>
2021-01-07 11:25:28 +0800

    v2

--- base/测试代码/抖音跳转.js
+++ base/测试代码/抖音跳转.js
@@ -1,7 +1,10 @@
 // com.hongshu.utils.IntentUtils.goKuaiShowHomePage(context)
 // com.hongshu.utils.IntentUtils.goKuaiShowUserPage(context,"3xmih2tpn2g2svg") 自动化工具
- //com.hongshu.utils.IntentUtils.goDouyinVideoPage(context,"6906053018739920140")
- com.hongshu.utils.IntentUtils.goDouyinUserPage(context,"4283310327999328")
+
+com.hongshu.utils.IntentUtils.goKuaiShowVideoPage(context,"3xc3p5uszvspd9y")// 
+
+//com.hongshu.utils.IntentUtils.goDouyinVideoPage(context,"6906053018739920140")
+//  com.hongshu.utils.IntentUtils.goDouyinUserPage(context,"4283310327999328")
 
 // let content=com.blankj.utilcode.util.CacheDiskUtils.getInstance("data_helpview").getString("data")
 // log(content)
