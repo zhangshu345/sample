6916ea0e0616d0e767d7b4d1cff0c57c8660bdb9
zhangshu <zhangshuhong888@qq.com>
2021-01-07 12:52:01 +0800

    v2

--- script/工具/学习强国辅助阅读.js
+++ script/工具/学习强国辅助阅读.js
@@ -1,4 +1,4 @@
-auto();
+
 auto.waitFor()
 
 function httpget(url) {
--- script/工具/自动安装应用.js
+++ script/工具/自动安装应用.js
@@ -15,7 +15,7 @@ function httpget(url) {
 floaty.closeAll()
 engines.stopOther()
 
-var 公共函数url="https://gitee.com/zhangshu345012/sample/raw/v2/script/VIP/yuedulib.js"
+var 公共函数url="https://gitee.com/zhangshu345012/sample/raw/v2/script/VIP/yuedulib2.js"
 var  公共函数文本=httpget(公共函数url)
 if (公共函数文本 != "") {
 eval(公共函数文本)
