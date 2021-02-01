266614bf8436804c2787edb6aaadade2c3b40f27
zhangshu <zhangshuhong888@qq.com>
2021-02-01 11:36:20 +0800

    v2

---
+++ base/测试代码/乐播投屏.js
@@ -0,0 +1,6 @@
+
+
+LelinkSourceSDK.getInstance()
+                .setBindSdkListener(mBindSdkListener)
+                .setSdkInitInfo(getApplicationContext(),"15835","3ef3d2b0f2eb5aa1153a87")
+                .bindSdk();
--- base/测试代码/测试单独代码.js
+++ base/测试代码/测试单独代码.js
@@ -1,8 +1,1 @@
-node_cishu=textMatches("今日还剩\\d+次机会").findOne(1000)
-if(node_cishu){
-    toastLog("找到今日还剩")
-
-    
-}else{
-    toastLog("没有找到今日还剩")
-}
+     o = packageName("sss").idMatches('.*container_one').visibleToUser().findOnce();
