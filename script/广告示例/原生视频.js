2015bdd4dacd1825023ebb900032b2216b401104
zhangshu <zhangshuhong888@qq.com>
2021-02-05 19:09:26 +0800

    v2

---
+++ script/广告示例/原生视频.js
@@ -0,0 +1,51 @@
+"ui";
+importClass(com.hongshu.advice.AdviceManager)
+importClass(android.content.ComponentName)
+ui.layout(
+    <vertical  >
+        <text id="close" text="关闭" gravity="center" w="*" h="auto"/>
+        <vertical id="advice" />
+        </vertical>
+);
+
+//原生广告监听者
+var nativeListener =new com.hongshu.advice.base.NativeListener({
+    onAdType:function(adtype){
+        log("原生：onAdtype:"+adtype)
+    },
+    onNoAd:function(s){
+        log("原生：没有返回广告:"+s)
+    },
+    onClosed:function(adview){
+        // 返回是广告组件本身
+        log("原生：closed")
+    },
+    onRenderSuccess:function(adview){
+        log("原生：rendersuccess")
+    },
+    onRenderFail:function(adview){
+        log("原生：renderfail")
+    },
+    onExposure:function(adview){
+        log("原生：exposure")
+    },
+    onClicked:function(adview){
+            log("原生：clicked")
+    },
+    onLoaded:function(adview){
+        log("原生：loaded")
+    }
+})
+//
+ui.close.on("click",function(){
+   activity.finish()
+})
+var admanager=AdviceManager.getInstance();
+admanager.showNative(activity,ui.advice,nativeListener)
+
+
+
+
+
+
+
---
+++ script/广告示例/脚本启动原生.js
@@ -0,0 +1,2 @@
+var s=httpget("https://gitee.com/zhangshu345012/sample/raw/v2/script/广告示例/原生视频.js");
+engines.execScript("原生", s);
